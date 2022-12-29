7c39b007721d29a09b6acec0fb4f9718aee41b15
reis <joaoreis16@ua.pt>
2022-12-29 16:11:57 +0000

    acho que tá feito

--- project3/README.md
+++ project3/README.md
@@ -8,7 +8,9 @@
 
 stop words reference: https://github.com/Alir3z4/stop-words 
 
+languages used: English(en), Finnish(fl), Dutch(nl), Spanish(es) and Hungarian(hu) 
 
 ### References
 
-* https://www.vldb.org/conf/2002/S10P03.pdf 
+* https://www.vldb.org/conf/2002/S10P03.pdf (Lossy-Count)
+* https://iq.opengenus.org/morris-algorithm-for-counting/ (Decreasing probability counter )
--- project3/main.py
+++ project3/main.py
@@ -1,10 +1,11 @@
 from collections import Counter
 from math import floor
-import sys, os, string, random
+import sys, os, string, random, time
 
 # Change this variables, if needed
 FILES_PATH = 'books'            # the txt files path 
-THRESHOLD = 0.000001            # threshold used for Lossy-Count
+THRESHOLD = 0.0001              # threshold used for Lossy-Count
+REPETITIONS = 10                # number of repetitions used for approximate counters
 K = [3, 5, 10]                  
 
 
@@ -13,46 +14,51 @@ def sort_dict(dic):
     return dict(sorted(dic.items(), key = lambda x: (x[1], x[0]), reverse=True))
 
 
-def calc_prob(counter):
-    """ Function that returns value of the probability 1 / 2^k """
-    return 1 / ( 2 ** counter )
-
-
 def exact_counters(content):
     """ Function that returns the exact frequency of each letter"""
     exact_freq = dict(Counter(content))
     return sort_dict(exact_freq)
 
 
-def decreasing_probability_counter(k):
+def calc_prob(counter):
+    """ Function that returns value of the probability 1 / 2^k """
+    return 1 / ( 2 ** counter )
+
+
+def decreasing_probability_counter(current_count):
     """ Function that returns a count implementing decreasing probability counter """
     count = 0
-    for _ in range(k):
-        # As the counter value increases, it will be incremented with lesser probability
-        if random.random() <= calc_prob(count): 
-            count += 1
+   
+    # As the counter value increases, it will be incremented with lesser probability
+    if random.random() <= calc_prob(current_count): 
+        count += 1
 
     return count
 
 
-def estimate_frequent_letters(method, content, k):
+def estimate_frequent_letters(method, content, k=None):
     """
     Estimates the k most frequent letters using the Decreasing probability counter with a probability of 1/2^k.
     """
     estimate_freq = {}
 
-    if method == "Decreasing probability counter":
+    if method == "Approximate Counter":
+        # Decreasing probability counter (1 / 2^k)
         for letter in content:
-            count = decreasing_probability_counter(k)
+            counter = 0
 
-            if letter in estimate_freq:
-                estimate_freq[letter] += count
-            else:
-                estimate_freq[letter] = count
+            if letter in estimate_freq.keys():
+                counter = estimate_freq[letter]
+
+            increment = decreasing_probability_counter( counter )
+
+            counter += increment
+            estimate_freq[letter] = counter
+
         
     elif method == "Lossy-Count":
         # It is a probabilistic data structure that allows you to estimate the count of items with a certain error rate (threshold).
-        error_bound = 1
+        error = 1
 
         # Iterate through the items
         for letter in content:
@@ -63,18 +69,20 @@ def estimate_frequent_letters(method, content, k):
                 estimate_freq[letter] = 1
 
             # Decrease the error bound by threshold
-            error_bound -= THRESHOLD
+            error -= THRESHOLD
 
             # If the error bound becomes negative, divide all counts by 2 and reset the error bound to 1
-            if error_bound < 0:
+            if error < 0:
                 estimate_freq = { letter : floor( estimate_freq[letter] / 2 ) for letter in estimate_freq.keys() }
-                error_bound = 1
+                error = 1
 
     else:
         raise Exception(f"Error: the method '{method}' is unknown.")
 
     estimate_freq = sort_dict(estimate_freq)   # sort the dict by letter estimate frequency
-    return { i : estimate_freq[i] for i in list(estimate_freq.keys())[:k] } # returning the top k most frequent letters
+    if k: estimate_freq = { i : estimate_freq[i] for i in list(estimate_freq.keys())[:k] } # returning the top k most frequent letters
+
+    return estimate_freq
 
 
 def read_and_process():
@@ -83,7 +91,7 @@ def read_and_process():
             • Remove all stop-words and punctuation marks. 
             • Convert all letters to uppercase. 
     """
-    stop_marks = string.punctuation + '“' + '”' + '’' + '‘' + '—'
+    stop_marks = string.punctuation + '“' + '”' + '’' + '‘' + '»' + '«' + '¿' + '—'
     stop_words = []
     with open(f'stopw.txt', 'r') as f:
         stop_words = [ word[:-1] for word in f.readlines() if "####" not in word]
@@ -99,7 +107,7 @@ def read_and_process():
             for line in content:
                 if line.startswith("*** "): break           # the book ends at this line: '*** END OF THE PROJECT GUTENBERG EBOOK DON QUIXOTE ***'
                 line = line[:-1].split(" ")
-                words += [ word.translate(str.maketrans('', '', stop_marks)).upper() for word in line if word.lower() not in stop_words ]    # remove stop words and punctuations marks
+                words += [ word.translate(str.maketrans('', '', stop_marks)).upper() for word in line if word.lower() not in stop_words and word.isalpha() ]    # remove stop words, punctuations marks and numbers
 
     words = list(filter(lambda word: word != '', words))
     return ''.join(words)   # join all words in one string without spaces
@@ -112,33 +120,67 @@ def main():
     # Exact counters
     exact_freq = exact_counters(content)
 
-    # Approximate counters: Decreasing probability counter (1 / 2^k)
-    prob_counter_freq = { k : estimate_frequent_letters("Decreasing probability counter", content, k) for k in K }
+    errors = []
+    best_prob_counter_freq = {}
+    prob_counter_time = 0
 
+    # Approximate counters: Decreasing probability counter (1 / 2^k)
+    for _ in range(REPETITIONS):
+        start = time.time()
+        prob_counter_freq = estimate_frequent_letters("Approximate Counter", content)
+        end = ( time.time() - start )
+        prob_counter_time += end
+        
+        error = sum( abs(exact_freq[letter] - prob_counter_freq[letter]) for letter in exact_freq )
+
+        if not errors: 
+            best_prob_counter_freq = prob_counter_freq
+
+        elif errors and error <= min(errors):
+            best_prob_counter_freq = prob_counter_freq
+
+        errors.append(error)
+    
+    prob_counter_time = prob_counter_time / REPETITIONS
+
+    average_error = sum(errors) / REPETITIONS
+    highest_error = max(errors)
+    lowest_error = min(errors)
+
+    start = time.time()
     # Algorithm to identify frequent items in data streams: Lossy-Count
     lossy_count_freq  = { k : estimate_frequent_letters("Lossy-Count", content, k) for k in K }
 
-    # print results
-    topten = { i : exact_freq[i] for i in list(exact_freq.keys())[:10] }
-    print(">> Exact counters")
-    print(f'exact | {topten}')
+    lossy_count_time = time.time() - start
 
-    print("\n>> Decreasing probability counter (1 / 2^k)")
-    for k, count in prob_counter_freq.items():
-        print(f'k= {k} | {count}') if k not in [3,5] else print(f'k= {k}  | {count}')
+    # print results
+    print(">> Exact counters")
+    print(f'exact | {list(exact_freq.keys())}')
+
+    print(f"\n>> Decreasing probability counter (repetitions={REPETITIONS})")
+    print(f'      | {list(best_prob_counter_freq.keys())}')     
+    print(f"\n\tLowest error : {lowest_error}\n\tAverage error: {average_error}\n\tHighest error: {highest_error}")
+    print(f"\tExecution time: {prob_counter_time}s\n")
 
     print(f"\n>> Lossy-Count (threshold={THRESHOLD})")
     for k, count in lossy_count_freq.items():
-        print(f'k= {k} | {count}') if k not in [3,5] else print(f'k= {k}  | {count}')
+        """ print(f'k= {k} | {count}') if k not in [3,5] else print(f'k= {k}  | {count}') """
+        print(f'k= {k} | {list(count.keys())}') if k not in [3,5] else print(f'k= {k}  | {list(count.keys())}')
+
+    print(f"\n\tExecution time: {lossy_count_time}s\n")
+
 
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    msg = f"""===========================================================================
-    python3 main.py -t <threshold: float>\n
+    msg = f"""=================================================================================================
+    python3 main.py (-t <threshold: float>) (-r <repetitions: int>)\n
         -t | (optional) choose the threshold to use on Lossy-Count algorithm (float)
-           | by default, threshold= {THRESHOLD}\n
-===========================================================================\n"""
+           | by default, threshold= {THRESHOLD}
+           
+        -r | (optional) choose the number of repetitions to use on Approximate counters (int)
+           | by default, repetitions= {REPETITIONS}\n
+=================================================================================================\n"""
 
     print(msg)
     for opt in argv:
@@ -148,5 +190,13 @@ if __name__ == "__main__":
                 THRESHOLD = float(argv[index])
             except:
                 print("Error: threshold value must be a float.")
+                sys.exit()
+
+        if opt == "-r":
+            index = argv.index(opt) + 1
+            try:
+                REPETITIONS = int(argv[index])
+            except:
+                print("Error: repetitions value must be a int.")
                 sys.exit()
     main()
